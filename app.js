7edb1656bc188dbaaa910e329b53bcaafa46120b
Fope Egbo <fope_egbo@hotmail.com>
2021-12-31 03:32:41 -0500

    Design Change 1

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ cart.addEventListener('click', function () {
         sideBar.style.display ="block";
         cart.innerHTML ="Continue Shopping";
     }else if(cart.innerHTML == "Continue Shopping")  {
-        sideBar.style.display = "none";
+        sideBar.style.display = "block";
         cart.innerHTML = "Add to cart";
     }
 });
 
 
+    // create a lopp where the add to cart btn changes to continue 
+    // shopping text when items have been added to the cart.  
 
-let 
+    // It changes to add to cart text when next item is selected
+
